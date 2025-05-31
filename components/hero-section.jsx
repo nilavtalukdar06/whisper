60a11a06f2b349f9197c3d97095459b3f727c2f0
Nilav <nilavtalukdar06@gmail.com>
2025-06-01 00:31:01 +0530

    added hero section

--- components/hero-section.jsx
+++ components/hero-section.jsx
@@ -1,12 +1,19 @@
+import { TextAnimate } from "./magicui/text-animate";
+
 export default function HeroSection() {
   return (
     <section className="max-w-4xl my-8 mx-auto p-10">
-      <h1 className="text-center text-4xl lg:text-6xl leading-[54px] md:leading-[72px]">
+      <TextAnimate
+        className="text-center text-4xl lg:text-6xl leading-[54px] md:leading-[72px]"
+        animation="slideUp"
+        by="line"
+        once
+      >
         Dive into the world of Anonymous Feedback
-      </h1>
-      <p className="text-center my-3 text-gray-500 text-sm lg:text-base">
+      </TextAnimate>
+      <TextAnimate className="text-center my-3 text-gray-500 text-sm lg:text-base">
         True Feedback - Where your identity remains a secret
-      </p>
+      </TextAnimate>
     </section>
   );
 }
