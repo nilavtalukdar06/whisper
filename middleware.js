da674fa27ec6cf6b61d4135e3f15ce8a5506e16c
Nilav <nilavtalukdar06@gmail.com>
2025-06-01 14:36:14 +0530

    configured middleware

---
+++ middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
+
+export async function middleware(request) {
+  const sessionCookie = getSessionCookie(request);
+  const { pathname } = request.nextUrl;
+  if (sessionCookie && ["/login", "/signup"].includes(pathname)) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+  if (!sessionCookie && pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard", "/login", "/signup"],
+};
