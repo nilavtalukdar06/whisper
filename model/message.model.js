10a4b4161df9323bb22fdecf5f0ddbdb56955b40
Nilav <nilavtalukdar06@gmail.com>
2025-06-01 16:50:38 +0530

    added mongoose model

---
+++ model/message.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: [true, "message is required"],
+      trim: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const messageModel =
+  mongoose.models.messageModel || mongoose.model("messageModel", messageSchema);
