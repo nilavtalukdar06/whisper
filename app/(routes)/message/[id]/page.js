a9fe53bb5b887d287348973556cf5a2d02d7804b
Nilav <nilavtalukdar06@gmail.com>
2025-06-01 20:53:30 +0530

    ui updates to the dynamic page

--- app/(routes)/message/[id]/page.js
+++ app/(routes)/message/[id]/page.js
@@ -1,8 +1,26 @@
+import { TextAnimate } from "@/components/magicui/text-animate";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+
 export default async function SendMessage({ params }) {
   const { id } = await params;
   return (
     <section className="min-h-screen max-w-screen overflow-x-hidden relative">
-      <h1>{id}</h1>
+      <div className="max-w-7xl mx-auto my-6 p-6">
+        <TextAnimate className="text-center text-3xl md:text-4xl font-semibold">
+          Public profile link
+        </TextAnimate>
+        <div className="max-w-5xl mx-auto my-10">
+          <Label htmlFor="message" className="mb-2 text-lg">
+            Send anonymous message
+          </Label>
+          <Textarea
+            id="message"
+            className="rounded shadow-none"
+            placeholder="Enter your anonymous message here"
+          />
+        </div>
+      </div>
     </section>
   );
 }
