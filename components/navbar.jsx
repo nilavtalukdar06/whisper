f79ebf7e96d68a4f3bcb2d61c38de134e355acc0
Nilav <nilavtalukdar06@gmail.com>
2025-06-01 15:42:55 +0530

    updated navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,6 +1,43 @@
+"use client";
+import toast from "react-hot-toast";
 import { Button } from "./ui/button";
+import { authClient } from "@/utils/auth-client";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import Spinner from "./spinner";
+import Link from "next/link";
 
 export default function Navbar() {
+  const router = useRouter();
+  const { data: session, isPending } = authClient.useSession();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const logout = async () => {
+    try {
+      const { error } = await authClient.signOut({
+        fetchOptions: {
+          onRequest: (ctx) => {
+            setIsLoading(true);
+          },
+          onSuccess: (ctx) => {
+            setIsLoading(false);
+            router.push("/");
+          },
+        },
+      });
+      if (error) {
+        throw new Error(error);
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Failed to logout");
+    }
+  };
+
+  useEffect(() => {
+    console.log(session);
+  }, [session]);
+
   return (
     <header className="px-5 md:px-24 py-8 w-full flex justify-between items-center">
       <div>
@@ -9,7 +46,24 @@ export default function Navbar() {
         </h1>
       </div>
       <div>
-        <Button>Login</Button>
+        {isPending ? (
+          <p className="text-xl font-medium">Loading...</p>
+        ) : session ? (
+          <Button disabled={isLoading} onClick={logout}>
+            {isLoading ? (
+              <div className="flex justify-center items-center">
+                <Spinner />
+                <p>Logging out...</p>
+              </div>
+            ) : (
+              "Logout"
+            )}
+          </Button>
+        ) : (
+          <Link href="/login">
+            <Button>Login</Button>
+          </Link>
+        )}
       </div>
     </header>
   );
