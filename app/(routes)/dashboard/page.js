59f3b2dc1df0300ba13aed0202273b9180f43ff0
Nilav <nilavtalukdar06@gmail.com>
2025-06-01 11:56:22 +0530

    added dashboard route

---
+++ app/(routes)/dashboard/page.js
@@ -0,0 +1,9 @@
+import Navbar from "@/components/navbar";
+
+export default function Dashboard() {
+  return (
+    <section className="min-h-screen max-w-screen overflow-x-hidden relative">
+      <Navbar />
+    </section>
+  );
+}
