764a675a659bd21d47624ad774e9fa237874e930
Nilav <nilavtalukdar06@gmail.com>
2025-06-01 21:44:35 +0530

    made a client component

---
+++ app/(routes)/_components/send-message.jsx
@@ -0,0 +1,24 @@
+"use client";
+import { TextAnimate } from "@/components/magicui/text-animate";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+
+export default function SendMessageComponent({ id }) {
+  return (
+    <div className="max-w-7xl mx-auto my-6 p-6">
+      <TextAnimate className="text-center text-3xl md:text-4xl font-semibold">
+        Public profile link
+      </TextAnimate>
+      <div className="max-w-5xl mx-auto my-10">
+        <Label htmlFor="message" className="mb-2 text-lg">
+          Send anonymous message {id}
+        </Label>
+        <Textarea
+          id="message"
+          className="rounded shadow-none"
+          placeholder="Enter your anonymous message here"
+        />
+      </div>
+    </div>
+  );
+}
--- app/(routes)/message/[id]/page.js
+++ app/(routes)/message/[id]/page.js
@@ -1,26 +1,10 @@
-import { TextAnimate } from "@/components/magicui/text-animate";
-import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
+import SendMessageComponent from "../../_components/send-message";
 
 export default async function SendMessage({ params }) {
   const { id } = await params;
   return (
     <section className="min-h-screen max-w-screen overflow-x-hidden relative">
-      <div className="max-w-7xl mx-auto my-6 p-6">
-        <TextAnimate className="text-center text-3xl md:text-4xl font-semibold">
-          Public profile link
-        </TextAnimate>
-        <div className="max-w-5xl mx-auto my-10">
-          <Label htmlFor="message" className="mb-2 text-lg">
-            Send anonymous message {id}
-          </Label>
-          <Textarea
-            id="message"
-            className="rounded shadow-none"
-            placeholder="Enter your anonymous message here"
-          />
-        </div>
-      </div>
+      <SendMessageComponent id={id} />
     </section>
   );
 }
