9dea60c37cc7c2b931b1851fc32ba31faa1d0d7c
Nilav <nilavtalukdar06@gmail.com>
2025-06-01 15:04:59 +0530

    completed the sign up functionality

--- app/(auth)/signup/page.js
+++ app/(auth)/signup/page.js
@@ -2,9 +2,13 @@
 import { TextAnimate } from "@/components/magicui/text-animate";
 import Spinner from "@/components/spinner";
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/utils/auth-client";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 export default function SignupPage() {
+  const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
   const [formData, setFormData] = useState({
     name: "",
@@ -12,13 +16,47 @@ export default function SignupPage() {
     password: "",
   });
 
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const { error } = await authClient.signUp.email(
+        {
+          name: formData.name,
+          email: formData.email,
+          password: formData.password,
+          callbackURL: "/dashboard",
+        },
+        {
+          onRequest: (ctx) => {
+            setIsLoading(true);
+          },
+          onSuccess: (ctx) => {
+            router.push("/dashboard");
+          },
+          onError: (ctx) => {
+            toast.error("Some error occured");
+          },
+        }
+      );
+      if (error) {
+        throw new Error(error);
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Some error occured");
+    }
+  };
+
   return (
     <section className="min-h-screen max-w-screen overflow-x-hidden">
       <div className="max-w-sm md:max-w-md mx-auto my-16 p-6">
         <TextAnimate className="text-3xl md:text-4xl font-normal text-center">
           Signup to Whisper
         </TextAnimate>
-        <form className="my-12 flex flex-col gap-y-6 justify-center items-center w-full">
+        <form
+          className="my-12 flex flex-col gap-y-6 justify-center items-center w-full"
+          onSubmit={handleSubmit}
+        >
           <div className="space-y-2 w-full flex flex-col">
             <label htmlFor="name">Full Name</label>
             <input
