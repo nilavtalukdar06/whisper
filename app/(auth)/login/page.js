cda2a18635bd2c2d09349748a1b74a3aab87e7d1
Nilav <nilavtalukdar06@gmail.com>
2025-06-01 14:56:55 +0530

    added loading ui to login and sign up screens

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -64,7 +64,7 @@ export default function LoginPage() {
               {isLoading ? (
                 <div className="flex gap-x-2 justify-center items-center">
                   <Spinner />
-                  <p>Loggin in...</p>
+                  <p>Logging in...</p>
                 </div>
               ) : (
                 "Login"
