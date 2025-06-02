78c1a64252f2f46c9a533572595a3fbc54adc570
Nilav <nilavtalukdar06@gmail.com>
2025-06-02 11:47:31 +0530

    added alert dialog component to delete messages

--- app/(routes)/_components/message-card.jsx
+++ app/(routes)/_components/message-card.jsx
@@ -1,3 +1,14 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { XIcon } from "lucide-react";
 
@@ -9,9 +20,28 @@ export default function MessageCard() {
           <h3 className="text-2xl">Hey, How are you?</h3>
           <p className="text-sm text-gray-600">May 31, 2025 8:10 P.M</p>
         </div>
-        <Button variant="destructive">
-          <XIcon />
-        </Button>
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button variant="destructive">
+              <XIcon />
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction className="bg-red-500 hover:bg-red-600">
+                Continue
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </div>
     </section>
   );
