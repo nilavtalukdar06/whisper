3f4b8e67e8ca09b3cc52bf6042cb5f7d0a805357
Nilav <nilavtalukdar06@gmail.com>
2025-06-05 14:42:34 +0530

    updated ui

--- app/(routes)/_components/message-card.jsx
+++ app/(routes)/_components/message-card.jsx
@@ -53,8 +53,7 @@ export default function MessageCard({
             <AlertDialogHeader>
               <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
               <AlertDialogDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
+                This will permanently delete this message
               </AlertDialogDescription>
             </AlertDialogHeader>
             <AlertDialogFooter>
