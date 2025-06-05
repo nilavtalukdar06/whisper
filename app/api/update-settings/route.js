642544bf2c88f545382a403292fe2f6712e1f4d8
Nilav <nilavtalukdar06@gmail.com>
2025-06-05 14:27:56 +0530

    updated a major api

--- app/api/update-settings/route.js
+++ app/api/update-settings/route.js
@@ -1,32 +1,46 @@
 import settingsModel from "@/model/settings.model";
 import { NextResponse } from "next/server";
+import connectToDb from "@/db/mongodb";
 
 export async function PUT(request) {
   try {
+    await connectToDb();
     const { user_id, accept_messages } = await request.json();
     if (!user_id) {
-      return NextResponse.json({
-        message: "user id is not present",
-      });
+      return NextResponse.json(
+        {
+          message: "user id is not present",
+        },
+        { status: 200 }
+      );
     }
     const result = await settingsModel.findOne({ user_id: user_id });
     if (!result) {
-      return NextResponse.json({
-        message: "settings not found",
-      });
+      return NextResponse.json(
+        {
+          message: "settings not found",
+        },
+        { status: 200 }
+      );
     } else {
       await settingsModel.updateOne(
         { user_id: user_id },
         { $set: { accept_messages: accept_messages } }
       );
-      return NextResponse.json({
-        message: "settings updated successfully",
-      });
+      return NextResponse.json(
+        {
+          message: "settings updated successfully",
+        },
+        { status: 201 }
+      );
     }
   } catch (error) {
     console.error(error.message);
-    return NextResponse.json({
-      message: `internal server error: ${error}`,
-    });
+    return NextResponse.json(
+      {
+        message: `internal server error: ${error}`,
+      },
+      { status: 500 }
+    );
   }
 }
